f5c526ad6863ae3b41757bd7cb0ff72ab45a0f97
nvalenzuela <nvalenzuela@SPLI-31-LC12>
2018-03-08 17:01:24 +0800

    Added jdbc Driver to the project

--- src/com/java/wms/connection.java
+++ src/com/java/wms/connection.java
@@ -1,5 +1,29 @@
 package com.java.wms;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class connection {
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "admin";
+	private static final String CONN_STRING = "jdbc:mysql://localhost/nvalen-wms";
 	
+	public static void main(String[] args) throws SQLException {
+		
+		//Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			System.err.println(e);
+		}finally {
+			if(conn != null) {
+				conn.close();
+			}
+		}
+	}
 }
